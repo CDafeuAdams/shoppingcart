7a5d70704823394d87827106d8e373cfde6c2fdf
CDafeuAdams <117130837+CDafeuAdams@users.noreply.github.com>
2022-11-12 16:00:44 -0600

    created a customer class, added a function and instance variables to the function

---
+++ customer.py
@@ -0,0 +1,7 @@
+
+class Customer:
+
+    def __init__(self, customers_name, shopping_cart, view_products)
+        self.name = customers_name
+        self.shopping_cart = shopping_cart
+        self.view_prodcuts = view_products
