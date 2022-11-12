a0b0291715a830682e20681fac150e19a7432a34
CDafeuAdams <117130837+CDafeuAdams@users.noreply.github.com>
2022-11-12 15:54:32 -0600

    created a product class and gave it a function and instance variables

---
+++ product.py
@@ -0,0 +1,7 @@
+
+class Product:
+
+    def __init__(self, products_name, products_price, products_category)
+        self.name = products_name
+        self.price = products_price
+        self.category = products_category
