b11ab54097f6836489dc3d54e0f5b83f35825416
CDafeuAdams <117130837+CDafeuAdams@users.noreply.github.com>
2022-11-12 15:58:11 -0600

    added a class for shoppingcart, a function and instance variables

---
+++ shoppingcart.py
@@ -0,0 +1,8 @@
+
+class Shopping_cart:
+
+    def __init__(self, shopping_cart_products, empty_products_from_shopping_cart, new_product, product_total)
+        self.shopping_cart = shopping_cart_products
+        self.total += product_total
+        self.new_product = new_product
+        self.empty.products = empty_products_from_shopping_cart
