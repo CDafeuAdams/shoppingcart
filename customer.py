372dcb34dc5d869052e649418fbfa53c76ab2c88
CDafeuAdams <117130837+CDafeuAdams@users.noreply.github.com>
2022-11-12 16:07:38 -0600

    imported shoppingcart into customer file

--- customer.py
+++ customer.py
@@ -1,7 +1,8 @@
+from shoppingcart import Shopping_cart
 
 class Customer:
 
     def __init__(self, customers_name, shopping_cart, view_products):
         self.name = customers_name
-        self.shopping_cart = shopping_cart
+        self.shopping_cart = Shopping_cart()
         self.view_prodcuts = view_products
