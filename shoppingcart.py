eeab7e708be05589a7871bef2c8f05e9c8259d91
CDafeuAdams <117130837+CDafeuAdams@users.noreply.github.com>
2022-11-12 16:02:15 -0600

    added a colon to the end of the function parameter

--- shoppingcart.py
+++ shoppingcart.py
@@ -1,7 +1,7 @@
 
 class Shopping_cart:
 
-    def __init__(self, shopping_cart_products, empty_products_from_shopping_cart, new_product, product_total)
+    def __init__(self, shopping_cart_products, empty_products_from_shopping_cart, new_product, product_total):
         self.shopping_cart = shopping_cart_products
         self.total += product_total
         self.new_product = new_product
